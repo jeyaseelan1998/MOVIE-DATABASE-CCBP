68686a264bc0f72877eda42649abe35c6fd859e2
jeyaseelan1998 <jeyaseelan.work@gmail.com>
2023-10-01 06:58:12 +0000

    initial commit

---
+++ src/components/Pagination/index.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import './index.css'
+
+class Pagination extends React.Component {
+  state = {
+    pageNo: 1,
+  }
+
+  onNextPage = () => {
+    const {apiCallback, totalPages} = this.props
+    this.setState(
+      prevState => {
+        if (prevState.pageNo < totalPages) {
+          return {
+            pageNo: prevState.pageNo + 1,
+          }
+        }
+        return prevState
+      },
+      () => {
+        const {pageNo} = this.state
+        apiCallback(pageNo)
+      },
+    )
+  }
+
+  onPrevPage = () => {
+    const {apiCallback} = this.props
+    this.setState(
+      prevState => {
+        if (prevState.pageNo > 1) {
+          return {
+            pageNo: prevState.pageNo - 1,
+          }
+        }
+        return prevState
+      },
+      () => {
+        const {pageNo} = this.state
+        apiCallback(pageNo)
+      },
+    )
+  }
+
+  render() {
+    const {pageNo} = this.state
+    const {totalPages} = this.props
+
+    return (
+      <div className="mb-3 d-flex justify-content-center align-items-center">
+        <button
+          type="button"
+          className="control-btn"
+          disabled={pageNo === 1}
+          onClick={this.onPrevPage}
+        >
+          prev
+        </button>
+        <p className="page-no">{pageNo}</p>
+        <button
+          type="button"
+          className="control-btn"
+          onClick={this.onNextPage}
+          disabled={pageNo === totalPages}
+        >
+          next
+        </button>
+      </div>
+    )
+  }
+}
+
+export default Pagination
